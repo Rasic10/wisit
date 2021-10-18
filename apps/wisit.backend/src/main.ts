125d67a9778be8997580f4f451b915b904dcfb6e
Nemanja Rasic <nemanja.rasic@inssio.com>
2021-10-19 00:26:06 +0200

    feat: introduced swagger

--- apps/wisit.backend/src/main.ts
+++ apps/wisit.backend/src/main.ts
@@ -1,21 +1,26 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Wisit app')
+    .setDescription('Api')
+    .setVersion('1.0')
+    .addTag('wisit')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document)
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
-  await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
-  });
+  
+  await app.listen(port);
 }
 
 bootstrap();
